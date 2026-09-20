cf56f4a7296d33a988b455453f23b9e001cb069f
Luiz Bills <luizbills@pm.me>
2026-09-20 12:32:16 -0300

    add movement with arrows

--- src/index.js
+++ src/index.js
@@ -14,14 +14,21 @@ const player = {
 display.draw(player.x, player.y, player.ch, null, null, 'You')
 
 const DIRS = {
+    // WASD
     w: [0, -1],
     s: [0, 1],
     a: [-1, 0],
     d: [1, 0],
+
+    // Arrows
+    arrowup: [0, -1],
+    arrowdown: [0, 1],
+    arrowleft: [-1, 0],
+    arrowright: [1, 0],
 }
 
 window.addEventListener('keydown', (ev) => {
-    const key = ev.key
+    const key = ev.key.toLowerCase()
     if (DIRS[key]) {
         const dx = DIRS[key][0]
         const dy = DIRS[key][1]
